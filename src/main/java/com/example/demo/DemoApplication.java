33c592d1f0c06985e331ee326c6fabf43428ac37
VijayababuBoopathy404 <56672491+VijayababuBoopathy404@users.noreply.github.com>
2019-10-17 13:08:53 +0400

    Comment Added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+   	/**
+	 *
+	 *  Spring Boot Main Method
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
